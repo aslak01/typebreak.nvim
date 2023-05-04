512544ffda8ddf82c9bfe7ffca97f8bff8460938
as <aslak.bakkeland@medialaben.no>
2023-05-04 08:30:41 +0200

    try to use usr/local/dict/word

--- lua/typebreak/state.lua
+++ lua/typebreak/state.lua
@@ -1,52 +1,44 @@
-local Path = require('plenary.path')
-local utils = require('typebreak.utils')
+local Path = require("plenary.path")
+local utils = require("typebreak.utils")
 
 local data_path = vim.fn.stdpath("data")
 local storage_path = string.format("%s/typebreak.json", data_path)
 
 local M = {
-    previous_times = {}
+	previous_times = {},
 }
 
-
 local load_json_from_path = function(path)
-    return vim.fn.json_decode(Path:new(path):read())
+	return vim.fn.json_decode(Path:new(path):read())
 end
 
 local store_state = function()
-    Path:new(storage_path):write(vim.fn.json_encode(M.previous_times), "w")
+	Path:new(storage_path):write(vim.fn.json_encode(M.previous_times), "w")
 end
 
 M.load = function()
-    local ok, result = pcall(load_json_from_path, storage_path)
-    if ok then
-        M.previous_times = result
-    end
+	local ok, result = pcall(load_json_from_path, storage_path)
+	if ok then
+		M.previous_times = result
+	end
 end
 
 M.record = function(time)
-    table.insert(M.previous_times, time)
-    store_state()
+	table.insert(M.previous_times, time)
+	store_state()
 end
 
 M.reset = function()
-    M.previous_times = {}
-    store_state()
+	M.previous_times = {}
+	store_state()
 end
 
 M.repr = function(time)
-    local last = #M.previous_times > 0
-        and M.previous_times[#M.previous_times]
-        or "N/A"
-    local avg = #M.previous_times > 0
-        and string.format("%.2f", (
-            (time + utils.table_sum(M.previous_times)
-                ) / (
-                #M.previous_times + 1)
-            ))
-        or "N/A"
-    return "Last: " .. last .. ', Avg: ' .. avg
+	local last = #M.previous_times > 0 and M.previous_times[#M.previous_times] or "N/A"
+	local avg = #M.previous_times > 0
+			and string.format("%.2f", ((time + utils.table_sum(M.previous_times)) / (#M.previous_times + 1)))
+		or "N/A"
+	return "Last: " .. last .. ", Avg: " .. avg
 end
-
 
 return M
--- lua/typebreak/utils.lua
+++ lua/typebreak/utils.lua
@@ -1,47 +1,51 @@
 local M = {}
 
 M.table_sum = function(table)
-    local sum = 0
-    for _, v in pairs(table) do
-        sum = sum + v
-    end
-    return sum
+	local sum = 0
+	for _, v in pairs(table) do
+		sum = sum + v
+	end
+	return sum
 end
 
 M.center_text = function(text, width)
-    return string.rep(' ', width / 2 - string.len(text) / 2) .. text
+	return string.rep(" ", width / 2 - string.len(text) / 2) .. text
 end
 
-local ns = vim.api.nvim_create_namespace('typebreak')
+local ns = vim.api.nvim_create_namespace("typebreak")
 
 M.highlight_text = function(row, col_start, col_end)
-    vim.api.nvim_buf_set_extmark(
-        0,
-        ns,
-        row,
-        col_start,
-        {
-            hl_group = 'ErrorMsg',
-            end_row = row,
-            end_col = col_end
-        }
-    )
+	vim.api.nvim_buf_set_extmark(0, ns, row, col_start, {
+		hl_group = "ErrorMsg",
+		end_row = row,
+		end_col = col_end,
+	})
 end
 
 M.reset_highlights = function()
-    vim.api.nvim_buf_clear_namespace(
-        0,
-        ns,
-        0,
-        -1
-    )
+	vim.api.nvim_buf_clear_namespace(0, ns, 0, -1)
 end
 
 M.extend_table = function(a, b)
-    for _, v in ipairs(b) do
-        table.insert(a, v)
-    end
-    return a
+	for _, v in ipairs(b) do
+		table.insert(a, v)
+	end
+	return a
+end
+
+M.file_exists = function(path)
+	local file = io.open(path, "rb")
+	if file then
+		file:close()
+	end
+	return file ~= nil
+end
+
+M.read_file = function(filename)
+	local fh = assert(io.open(filename, "rb"))
+	local contents = assert(fh:read(_VERSION <= "Lua 5.2" and "*a" or "a"))
+	fh:close()
+	return contents
 end
 
 return M
